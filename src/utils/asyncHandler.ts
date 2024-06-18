0c8d7e8219f7fe43b35a0f01a3eda2840e1bb8f8
samrat-gh <samratghimire32@gmail.com>
2024-06-18 22:17:01 +0545

    add asyncHandler

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+const asyncHandler = async (
+  fun: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fun(req, res, next);
+    } catch (err) {
+      res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+};
+
+export default asyncHandler;
