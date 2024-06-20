2717e8d69a88b357f70cba18a01c3055066ffef4
samrat-gh <samratghimire32@gmail.com>
2024-06-20 23:21:43 +0545

    update day 20 with schema and auth packages

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,50 +1,83 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    index: true,
-    trim: true,
-  },
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+      trim: true,
+    },
 
-  fullname: {
-    type: String,
-    required: true,
-    index: true,
-    trim: true,
-  },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
 
-  avatar: {
-    type: String, //Cloudinary URL
-    required: true,
-  },
+    fullname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
 
-  coverImage: {
-    type: String,
+    password: {
+      type: String,
+      required: [true, "Password cannot be empty"],
+    },
+    refreshTokem: {
+      type: String,
+    },
   },
+  {
+    timestamps: true,
+  }
+);
 
-  watchHistory: {
-    type: Schema.Types.ObjectId,
-    ref: "Video",
-  },
-  password: {
-    type: String,
-    required: [true, "Password cannot be empty"],
-  },
-  refreshTokem: {
-    type: String,
-  },
+//no arrow function because it doesn't have refrence to this keyword
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await brcypt.hash(this.password, 10);
+  next();
 });
 
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+  return await brcypt.compare(password, this.password);
+};
+
+userSchema.methods.accessToken = function () {
+  return jwt.sign(
+    {
+      _id: this.id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_SECRET,
+    }
+  );
+};
+
+userSchema.methods.refreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_SECRET,
+    }
+  );
+};
 export const User = mongoose.model("Users", userSchema);
